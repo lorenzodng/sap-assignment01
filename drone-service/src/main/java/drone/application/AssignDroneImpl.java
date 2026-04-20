d9b0150d8f5e78d4b8cacf4e7d3586fb875927c8
Lorenzo <lorenzodangel00@gmail.com>
2026-04-20 02:13:43 +0200

    remove comments

--- drone-service/src/main/java/drone/application/AssignDroneImpl.java
+++ drone-service/src/main/java/drone/application/AssignDroneImpl.java
@@ -14,14 +14,13 @@ public class AssignDroneImpl implements AssignDrone {
 
     @Override
     public Drone assign(List<Drone> drones, double packageWeight, double pickupLatitude, double pickupLongitude, double distancePickupToDelivery, int deliveryTimeLimit) {
-
         return drones.stream().filter(Drone::isAvailable).filter(drone -> {
-                    double distanceDroneToPickup = GeoUtils.haversine(drone.getPosition().getLatitude(), drone.getPosition().getLongitude(), pickupLatitude, pickupLongitude);
-                    return checkDroneAvailability.check(drone, packageWeight, distanceDroneToPickup, distancePickupToDelivery, deliveryTimeLimit);
-                }).min((d1, d2) -> {
-                    double dist1 = GeoUtils.haversine(d1.getPosition().getLatitude(), d1.getPosition().getLongitude(), pickupLatitude, pickupLongitude);
-                    double dist2 = GeoUtils.haversine(d2.getPosition().getLatitude(), d2.getPosition().getLongitude(), pickupLatitude, pickupLongitude);
-                    return Double.compare(dist1, dist2);
-                }).orElseThrow(DroneNotAvailableException::new);
+            double distanceDroneToPickup = GeoUtils.haversine(drone.getPosition().getLatitude(), drone.getPosition().getLongitude(), pickupLatitude, pickupLongitude);
+            return checkDroneAvailability.check(drone, packageWeight, distanceDroneToPickup, distancePickupToDelivery, deliveryTimeLimit);
+        }).min((d1, d2) -> {
+            double dist1 = GeoUtils.haversine(d1.getPosition().getLatitude(), d1.getPosition().getLongitude(), pickupLatitude, pickupLongitude);
+            double dist2 = GeoUtils.haversine(d2.getPosition().getLatitude(), d2.getPosition().getLongitude(), pickupLatitude, pickupLongitude);
+            return Double.compare(dist1, dist2);
+        }).orElseThrow(DroneNotAvailableException::new);
     }
 }
